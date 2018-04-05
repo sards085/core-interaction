8acc7863e381da8292ac76a710bde1f2dc2a2979
Sav <savannasardi@Savannas-MacBook-Pro.local>
2018-04-05 17:52:10 -0400

    ok

--- studio/assets/js/hp.js
+++ studio/assets/js/hp.js
@@ -1,4 +1,17 @@
-// PAGE SPECIFIC JS
-// Delete this file if you don't need it
+var circle = document.querySelector('.circle')
 
-console.log('This the index.')
+	circle.isActive = false
+	circle.addEventListener('mousedown', function() {
+		 circle.isActive = true
+	})
+
+window.addEventListener('mouseup', function() {
+		circle.isActive = false
+	})
+
+window.addEventListener('mousemove', function(event) {
+		if (circle.isActive) {
+			circle.style.top = event.pageY - 20 + 'px'
+			circle.style.left = event.pageX - 20 + 'px'
+		}
+	})
